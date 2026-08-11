8a1a2611f9863b85664abf3f86e1c919000ddaf7
celin <celinabenke@alunos.utfpr.edu.br>
2026-08-11 09:13:24 -0300

    [main] configurar_ change projeto1.py

--- projeto1.py
+++ projeto1.py
@@ -8,67 +8,101 @@ ser = serial.Serial(
 def cmd(comando):
     ser.write((comando + "\n").encode())
 
-def configurar():
+def calibrar():
+    print("\n--- CALIBRATION ---")
+    calibracoes = {
+        "open": {
+            "magnitude": "1",
+            "comando": "calibrate open"
+        },
 
+        "short": {
+            "magnitude": "0.2",
+            "comando": "calibrate short"
+        },
+
+        "load": {
+            "magnitude": "1",
+            "comando": "calibrate load"
+        }
+    }
+    escolha = input(
+        "Qual calibração deseja fazer? "
+        "(open, short, load ou todas): "
+    ).lower()
+
+    if escolha == "todas":
+        for nome, configuracao in calibracoes.items():
+            input(f"Conecte o {nome.upper()} e pressione ENTER")
+            cmd(f"magnitude {configuracao['magnitude']}")
+            cmd(configuracao["comando"])
+    else:
+        configuracao = calibracoes[escolha]
+        input(f"Conecte o {escolha.upper()} e pressione ENTER")
+
+        cmd(f"magnitude {configuracao['magnitude']}")
+        cmd(configuracao["comando"])
+
+def configurar_inicial():
+
+    ch0 = input("Setgain ch0: ")
+    ch1 = input("Setgain ch1: ")
+    magnitude = input("Magnitude: ")
+    offset = input("Offset: ")
+    average = input("Average: ")
+    delay = input("Trigger delay: ")
+    frequency = input("Calibration frequency: ")
+
+    cmd(f"setgain ch0 {ch0}")
+    cmd(f"setgain ch1 {ch1}")
+    cmd(f"magnitude {magnitude}")
+    cmd(f"offset {offset}")
+    cmd(f"average {average} ")
+    cmd(f"tdelay {delay}")
+    cmd(f"frequency {frequency}")
+
+def configurar_sweep():
+    print("\n--- SWEEP CONFIGURATION ---")
     display = input("Display: ") or "9"
-    ch0 = input("Setgain ch0: ") or "0"
-    ch1 = input("Setgain ch1: ") or "1"
-    magnitude = input("Magnitude: ")or "1"
     count = input("Count: ") or "136"
 
     sweep_type = input("Sweep Type Frequency: ") or "1 1000"
-    inico,fim = sweep_type.split()
+    inico, fim = sweep_type.split()
     sweep_scale = input("Sweep Type Scale: ") or "log"
 
     cmd(f"display {display}")
-    cmd(f"ch0 {ch0}")
-    cmd(f"ch1 {ch1}")
-    cmd(f"magnitude {magnitude}")
     cmd(f"count {count}")
     cmd(f"sweep_type frequency {inico} {fim}")
     cmd(f"sweep_scale {sweep_scale}")
 
-def medir():
+    return int(count)
+
+def medir(count):
     cmd("z")
 
     medidas = []
 
-    for _ in range(136):
+    for _ in range(count):
         medidas.append(ser.readline().decode().strip())
 
     return medidas
 
 def main ():
-    configurar()
-    return medir()
 
-if __name__ == "__main__":
-    dados = main()
+    configurar_inicial()
 
-    medidas = main()
+    calibrado = input("O equipamento já foi calibrado? [s/n]: ")
 
-    for linha in medidas:
-        print(linha)
+    if calibrado.lower() != "s":
+        calibrar()
 
+    count = configurar_sweep()
 
-#short
-##Short calibration can be performed only when gain channel 1 is set to 0 or 1. When channel 1 is in gain 1, the magnitude of the source must be less than 0.2Vpk.
-#a unica diferença entre os 3 é a magnitude do short
+    medidas = medir(count)
 
+    return medidas
 
-##ser.write(b"setgain ch0 \r\n")
-#ouu se usa cmd
+medidas = main()
 
-
-#coisas que da pra mudar
-##display
-##count
-##sweep_type frequency 1 1000
-##?delay
-##magnitude
-##ch0 e ch1
-##sweep_scale
-##??frequency
-##average
-##?offset
-
+for linha in medidas:
+    print(linha)
