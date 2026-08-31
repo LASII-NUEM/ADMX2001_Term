2f5cbbb44a7025768d22731ef87e4785856ae21a
Everton <evertonjunior@alunos.utfpr.edu.br>
2026-08-31 11:17:27 -0300

    [main] commit framework changes

---
+++ ADMX_Meas.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# ADMX2001 Terminal Framework
+# =============================================================================
+# File:
+#   admx_meas.py
+#
+# Description:
+#   Implements the measurement procedure for the Analog Devices
+#   EVAL-ADMX2001. This module configures the measurement parameters,
+#   performs the requested measurements, and acquires the resulting data.
+#
+# Author:
+#   Everton Trento Jr.
+#
+# Institution:
+#   NUEM - Multiphase Flow Research Center
+#   UTFPR - Federal University of Technology - Paraná
+#
+# Hardware:
+#   Analog Devices EVAL-ADMX2001
+#
+# Language:
+#   Python
+#
+# =============================================================================
---
+++ ADMX_calib.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# ADMX2001 Terminal Framework
+# =============================================================================
+# File:
+#   admx_calib.py
+#
+# Description:
+#   Implements the full-spectrum calibration procedure for the Analog Devices
+#   EVAL-ADMX2001. This module uses the calibration utilities provided by the
+#   calibrate_utils.py class to perform and manage the calibration process.
+#
+# Author:
+#   Everton Trento Jr.
+#
+# Institution:
+#   NUEM - Multiphase Flow Research Center
+#   UTFPR - Federal University of Technology - Paraná
+#
+# Hardware:
+#   Analog Devices EVAL-ADMX2001
+#
+# Language:
+#   Python
+# =============================================================================
+
